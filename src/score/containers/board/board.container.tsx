c35e529e5917d2dfebd70e75b678dcd7dd997b55
adrianTM <adrian.lopez@talentomobile.com>
2021-10-12 16:23:36 +0200

    [UPD] previous commit

--- src/score/containers/board/board.container.tsx
+++ src/score/containers/board/board.container.tsx
@@ -3,22 +3,20 @@ import { MatchLine } from '@components';
 import './board.container.css';
 import React from 'react';
 
-export const BoardContainer = React.memo(({ matches }: iProps): JSX.Element => {
-    return (
-        <main className='board-container'>
-        <h3 className='board-title'>Matches Board</h3>
-            {matches?.length ? (
-                <>
-                    {matches?.map((match) => {
-                        return <MatchLine key={match.inputDate.toISOString()} match={match} />
-                    })}
-                </>
-            ):(
-                <span className='board-empty'>Ups, this looks like empty!</span>
-            )}
-        </main>
-    )
-});
+export const BoardContainer = React.memo(({ matches }: iProps): JSX.Element => (
+    <main className='board-container'>
+    <h3 className='board-title'>Matches Board</h3>
+        {matches?.length ? (
+            <>
+                {matches?.map((match) => {
+                    return <MatchLine key={match.inputDate.toISOString()} match={match} />
+                })}
+            </>
+        ):(
+            <span className='board-empty'>Ups, this looks like empty!</span>
+        )}
+    </main>
+));
 
 interface iProps {
     matches?: iMatchExtended[];
